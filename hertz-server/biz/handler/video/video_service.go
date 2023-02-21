842d9917d1bee4f0f31ee6845d3ae627d9d05bcc
2695915190@qq.com <kkkJoeyyy>
2023-02-22 05:09:29 +0800

    mw & playUrl problem

--- hertz-server/biz/handler/video/video_service.go
+++ hertz-server/biz/handler/video/video_service.go
@@ -118,7 +118,7 @@ func VideoPublish(ctx context.Context, c *app.RequestContext) {
 		return
 	}
 
-	playUrl := "http://43.139.145.135:7777/public" + fileName + ".mp4"
+	playUrl := "http://43.139.145.135:7777/public/" + fileName + ".mp4"
 	println("playUrl: ", playUrl)
 	// test
 	err = dal.Video.WithContext(ctx).Create(&model.Video{UID: 1, PlayURL: playUrl, CoverURL: "coverUrl"})
@@ -137,6 +137,9 @@ func VideoPublish(ctx context.Context, c *app.RequestContext) {
 // GetPublishList .
 // @router /douyin/publish/list [GET]
 func GetPublishList(ctx context.Context, c *app.RequestContext) {
+
+	// todo
+	// resp是正常的，应该是playUrl有问题 所以播放不了
 	var err error
 	var req video.DouyinPublishListRequest
 	err = c.BindAndValidate(&req)
@@ -145,12 +148,14 @@ func GetPublishList(ctx context.Context, c *app.RequestContext) {
 		return
 	}
 	// jwt授权，从token中获取uid
-	u, _ := c.Get(mw.JwtMiddleware.IdentityKey)
-	println("video list:", u.(*mw.Claim).ID, u.(*mw.Claim).Username)
+	// u, _ := c.Get(mw.JwtMiddleware.IdentityKey)
+	// println("video list:", u.(*mw.Claim).ID, u.(*mw.Claim).Username)
+
+	uid := req.UserId
 
 	resp := new(video.DouyinFavoriteListResponse)
 
-	data, err := dal.Video.Where(dal.Video.UID.Eq(u.(*mw.Claim).ID)).Find()
+	data, err := dal.Video.Where(dal.Video.UID.Eq(uid)).Find()
 	if err != nil {
 		println("query database failed")
 		return
@@ -161,9 +166,12 @@ func GetPublishList(ctx context.Context, c *app.RequestContext) {
 	for _, d := range data {
 		var tmp video.Video
 		tmp.Id = d.Vid
-		tmp.CoverUrl = d.CoverURL
+		tmp.CoverUrl = d.PlayURL
 		tmp.PlayUrl = d.PlayURL
 		// 还有很多没有实现的
+
+		fmt.Println(tmp)
+		fmt.Println("vid: ", d.Vid)
 		v = append(v, &tmp)
 	}
 	resp.VideoList = v
@@ -186,6 +194,8 @@ func GetFeed(ctx context.Context, c *app.RequestContext) {
 	// 30 videos for a single time
 	limit := 30
 	// data, err := dal.Video.Order("created_at desc").Limit(limit).Find()
+	// todo
+	// 这里不知道Order不知道咋放函数
 	data, err := dal.Video.Limit(limit).Find()
 	if err != nil {
 		println("query database failed")
